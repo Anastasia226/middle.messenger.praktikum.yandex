5b7a2cfa1680713a3dbcec0ae74da81d08dfb231
Anastasia Bagaeva <72441267+Anastasia226@users.noreply.github.com>
2022-05-13 22:33:48 +0300

    Sprint 1 (#2)
    * add autorization
    
    * add registration
    
    * delete dist from gitignore
    
    * add profile
    
    * add edit pages
    
    * add page chats
    
    * add page chats
    
    * fix
    
    * fix
    
    * delete engine in package
    
    * fix
    
    * add dist
    
    * fix index.js
    
    * fix
    
    * fix readme
    
    * Delete node_modules directory
    
    * add commits
    
    Co-authored-by: Anastasiia <aabagaeva>

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
+
+# О проекте
+
+веб-мессенджер
+
+# Дизайн
+
+https://www.figma.com/file/hwwXd8HxXnJA5DXD9ZhDLJ/chat-parcticum?node-id=9%3A2
+
+# Установка
+
+npm install
+
+# Запустить
+
+npm run start
+=======
 # sprint_1
 https://github.com/Anastasia226/sprint_1/pull/2
---
+++ src/utils/mydash/range.js
@@ -0,0 +1,20 @@
+function range(start = 0, end, step = 1, isRight = false) {
+    if (end === undefined) {
+        end = start;
+        start = 0;
+    }
+    if (step === 0) {
+        return new Array(end - start).fill(undefined).map(() => start);
+    }
+    if (isRight) {
+
+        if (end < 0) {
+            return new Array((0 - end) - start).fill(undefined).map((_, index) => start - index * step).reverse();
+        }
+        return new Array((end - start) / step).fill(undefined).map((_, index) => start + index * step).reverse();
+    }
+    if (end < 0) {
+        return new Array((0 - end) - start).fill(undefined).map((_, index) => start - index * step);
+    }
+    return new Array((end - start) / step).fill(undefined).map((_, index) => start + index * step);
+} 
