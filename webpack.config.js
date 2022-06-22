ae5fe7ef2f34151c75e6e993435514e09f82e31d
Anastasiia <aabagaeva>
2022-06-22 21:57:52 +0300

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'project-name.bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 4000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: path.resolve(__dirname, 'tsconfig.json'),
+            },
+          },
+        ],
+        exclude: /(node_modules)/,
+      },
+    ],
+  },
+};
